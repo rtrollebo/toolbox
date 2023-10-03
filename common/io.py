7636461be345464ecb83c9c400a7b62d84933b2a
Robert Trollebø <rtrollebo@gmail.com>
2023-10-03 21:26:57 +0200

    Add a custom exception for handle io errors.

--- common/io.py
+++ common/io.py
@@ -1,4 +1,5 @@
 import yaml
+from common.exception import APITesterIOException
 
 
 def read_file_yaml(fname):
@@ -6,7 +7,10 @@ def read_file_yaml(fname):
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
-            print(exc)
+            raise APITesterIOException("Failed to read {0} due to an parsing error: {1}".format(fname, exc))
+        except Exception as e:
+            raise APITesterIOException("Failed to read {0}: {1}".format(fname, e))
+
 
 
 def write_file_yaml(fname, d):
@@ -14,4 +18,6 @@ def write_file_yaml(fname, d):
         with open(fname, 'w') as stream:
             yaml.dump(d, stream, default_flow_style=False)
     except yaml.YAMLError as exc:
-        print("problem")
+        raise APITesterIOException("Failed to write {0} due to an yaml error: {1}".format(fname, exc))
+    except Exception as e:
+        raise APITesterIOException("Failed to write {0}: {1}".format(fname, e))
