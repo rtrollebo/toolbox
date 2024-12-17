0fc7dfaf9a90c9b32426066b5f85a76bcda88c99
robert <rtrollebo@gmail.com>
2024-12-17 17:59:06 +0100

    Use optional argument list for tool-specific arguments

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from userstat.main import main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="ToolBox")
     parser.add_argument('tool')
+    parser.add_argument('--args', nargs='+', help='Optional tool arguments', default=[])
     args = parser.parse_args()
+    print(args.args)
     match args.tool:
         case 'userstat':
-            main()
+            main(args.args)
         case _:
             print(f"Unknown tool: {args.tool}")
